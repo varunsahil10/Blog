3be4d64688fdcb87f9119f7bc75927348a3e213f
varunsahil10 <varunsahil10@gmail.com>
2017-07-08 12:49:12 +0530

    completed show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,18 @@ class ArticlesController < ApplicationController
 	def create
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to_aticles_show(@article)
+		if @article.save
+			flash[:notice] = "Article created successfully"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
 
+	end
+
+	def show
+		@article = Article.find(params[:id])
+		
 	end
 
 	private
